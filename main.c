e67e6346a3efb4a1c9ea042fce95cb3c629b9d3a
Nathan Henrique <nscjamp6@gmail.com>
2025-05-10 21:38:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,41 @@ int main() {
     printf("Digite um número: ");
     scanf("%d", &num);
 
+    if (num % 1 == 1) {
+        printf("%d é ímpar.\n", num);
+    }
+
     if (num % 2 == 0) {
         printf("%d é par.\n", num);
-    } else {
+    }
+    
+    if (num % 3 == 1) {
+        printf("%d é ímpar.\n", num);
+    }
+
+    if (num % 4 == 0) {
+        printf("%d é par. \n", num);
+    }
+
+     if (num % 5 == 1) {
+        printf("%d é ímpar.\n", num);
+    }
+
+    if (num % 6 == 0) {
+        printf("%d é par.\n", num);
+    }
+    
+    if (num % 7 == 1) {
+        printf("%d é ímpar.\n", num);
+    }
+
+    if (num % 8 == 0) {
+        printf("%d é par. \n", num);
+    }
+
+     if (num % 9 == 1) {
         printf("%d é ímpar.\n", num);
     }
 
     return 0;
-}
+
